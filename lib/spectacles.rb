4f662c3870f87a882f531c2e61f33a0ed9b53233
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-02-05 12:12:10 -0700

    alias inherited if already defined

--- lib/spectacles.rb
+++ lib/spectacles.rb
@@ -3,8 +3,11 @@ require 'spectacles/schema_statements'
 require 'spectacles/version'
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
+  alias_method(:_spectacles_orig_inherited, :inherited) if method_defined?(:inherited)
+
   def self.inherited(klass)
     Spectacles::load_adapters
+    _spectacles_orig_inherited if method_defined?(:_spectacles_orig_inherited)
   end
 end
 
