7591ee21ebaf5753a32b5a1ed5bedefd6efa3800
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-02-04 20:45:34 -0700

    move to abstract adapter model

--- lib/spectacles.rb
+++ lib/spectacles.rb
@@ -1,5 +1,6 @@
 require 'active_record'
 require 'spectacles/schema_statements'
+require 'spectacles/schema_statements/abstract_adapter'
 require 'spectacles/version'
 
 # TODO: check adapter type before loading
@@ -7,7 +8,7 @@ require 'spectacles/schema_statements/sqlite_adapter'
 require 'active_record/connection_adapters/sqlite_adapter'
 
 ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do 
-  include Spectacles::SchemaStatements
+  include Spectacles::SchemaStatements::AbstractAdapter
 end
 
 ActiveRecord::ConnectionAdapters::SQLiteAdapter.class_eval do 
