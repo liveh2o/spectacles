419affb58a3093b37865c66c8fb154f35b731131
Adam Hutchison <adam.hutchison@moneydesktop.com>
2012-02-06 15:27:41 -0700

    Don't need to reload the database for the view examples

--- specs/adapters/postgresql_adapter_spec.rb
+++ specs/adapters/postgresql_adapter_spec.rb
@@ -13,8 +13,5 @@ describe "Spectacles::SchemaStatements::PostgreSQLAdapter" do
   load_schema
 
   it_behaves_like "an adapter", "PostgreSQLAdapter"
-  
-  recreate_database("spectacles_test")
-  load_schema
   it_behaves_like "a view model"
 end
