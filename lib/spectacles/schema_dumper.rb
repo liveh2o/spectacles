e68715b49d221927befdd64e748f1910cf820f24
Adam Hutchison <adam@moneydesktop.com>
2012-02-10 13:56:18 -0700

    Fixed formatting when views are dumped to the schema

--- lib/spectacles/schema_dumper.rb
+++ lib/spectacles/schema_dumper.rb
@@ -8,9 +8,9 @@ module Spectacles
 
     def self.dump_view(stream, connection, view_name)
       stream.print <<-CREATEVIEW
-        create_view :#{view_name} do 
-          "#{connection.view_build_query(view_name)}"
-        end
+  create_view :#{view_name} do
+    "#{connection.view_build_query(view_name)}"
+  end
       CREATEVIEW
     end
   end
