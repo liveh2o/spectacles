52a59ee4321a77823873972b4850b44a594a35fb
Adam Hutchison <liveh2o@gmail.com>
2013-09-26 17:31:02 -0600

    Run AS load hooks when the view is loaded.

--- lib/spectacles/view.rb
+++ lib/spectacles/view.rb
@@ -1,9 +1,11 @@
 module Spectacles
   class View < ActiveRecord::Base
     self.abstract_class = true
-    
+
     def readonly?
       true
     end
   end
+
+  ::ActiveSupport.run_load_hooks(:spectacles, View)
 end
