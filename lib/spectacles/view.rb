7e98a077daf3fb80cadd78bea0a75228729fd7d9
Adam Hutchison <liveh2o@gmail.com>
2013-09-27 11:38:32 -0600

    Add view_exists? to View, alias it to table_exists?

--- lib/spectacles/view.rb
+++ lib/spectacles/view.rb
@@ -2,6 +2,15 @@ module Spectacles
   class View < ActiveRecord::Base
     self.abstract_class = true
 
+    def self.view_exists?
+      self.connection.view_exists?(self.view_name)
+    end
+
+    class << self
+      alias_method :table_exists?, :view_exists?
+      alias_method :view_name, :table_name
+    end
+
     def readonly?
       true
     end
