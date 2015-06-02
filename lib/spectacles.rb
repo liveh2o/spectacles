521ba9e2e8a01f68257b1b68b682afb9a8489fde
Steve Newell <steve.newell@mx.com>
2015-06-02 14:37:58 -0600

    use quote_table_name helper

--- lib/spectacles.rb
+++ lib/spectacles.rb
@@ -44,8 +44,8 @@ ActiveRecord::ConnectionAdapters::SchemaStatements.class_eval do
   alias_method(:_spectacle_drop_table, :drop_table) if method_defined?(:drop_table)
 
   # Add this so that views get cleaned up properly
-  def drop_table(name, options={})
-    execute "DROP TABLE #{name} CASCADE"
+  def drop_table(table_name, options={})
+    execute "DROP TABLE #{quote_table_name(table_name)} CASCADE"
   end
 end
 
