47eed7fb6bc8733d19eac5cbf5df6ecb2a024db6
Jamis Buck <jamisbuck@gmail.com>
2015-09-24 10:18:12 -0600

    Fix broken MySQL and SQLite tests
    * Retrain #tables method on SQLite and MySQL adapters to return only tables
    * use Module#prepend to load #tables overrides properly
    * remove sqlite adapter as it is no longer supported in ActiveRecord

--- lib/spectacles/schema_statements/mysql_adapter.rb
+++ lib/spectacles/schema_statements/mysql_adapter.rb
@@ -4,7 +4,26 @@ module Spectacles
   module SchemaStatements
     module MysqlAdapter
       include Spectacles::SchemaStatements::AbstractAdapter
-      
+
+      # overrides the #tables method from ActiveRecord's MysqlAdapter
+      # to return only tables, and not views.
+      def tables(name = nil, database = nil, like = nil)
+        database = database ? quote_table_name(database) : "DATABASE()"
+        by_name  = like ? "AND table_name LIKE #{quote(like)}" : ""
+
+        sql = <<-SQL.squish
+          SELECT table_name, table_type
+            FROM information_schema.tables
+           WHERE table_schema = #{database}
+             AND table_type = 'BASE TABLE'
+             #{by_name}
+        SQL
+
+        execute_and_free(sql, 'SCHEMA') do |result|
+          result.collect(&:first)
+        end
+      end
+
       def views(name = nil) #:nodoc:
         execute("SHOW FULL TABLES WHERE TABLE_TYPE='VIEW'").map { |row| row[0] }
       end
