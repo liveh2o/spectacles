da8dd8f0328a62d6148d59f49bd5f39d65c1bfa6
Mark Oveson <mark.oveson@mx.com>
2019-12-13 14:20:33 -0700

    Linting

--- specs/support/schema_statement_examples.rb
+++ specs/support/schema_statement_examples.rb
@@ -1,18 +1,18 @@
-require 'spec_helper'
+require "spec_helper"
 
 shared_examples_for "an adapter" do |adapter|
   shared_base = Class.new do
     extend Spectacles::SchemaStatements.const_get(adapter)
     def self.quote_table_name(name); name; end
     def self.quote_column_name(name); name; end
-    def self.execute(query); query; end 
+    def self.execute(query); query; end
   end
 
-  describe "ActiveRecord::SchemaDumper#dump" do 
+  describe "ActiveRecord::SchemaDumper#dump" do
     before(:each) do
       ActiveRecord::Base.connection.drop_view(:new_product_users)
 
-      ActiveRecord::Base.connection.create_view(:new_product_users) do 
+      ActiveRecord::Base.connection.create_view(:new_product_users) do
         "SELECT name AS product_name, first_name AS username FROM
         products JOIN users ON users.id = products.user_id"
       end
@@ -21,7 +21,7 @@ shared_examples_for "an adapter" do |adapter|
         ActiveRecord::Base.connection.drop_materialized_view(:materialized_product_users)
         ActiveRecord::Base.connection.drop_materialized_view(:empty_materialized_product_users)
 
-        ActiveRecord::Base.connection.create_materialized_view(:materialized_product_users, force: true) do 
+        ActiveRecord::Base.connection.create_materialized_view(:materialized_product_users, force: true) do
           "SELECT name AS product_name, first_name AS username FROM
           products JOIN users ON users.id = products.user_id"
         end
@@ -35,20 +35,20 @@ shared_examples_for "an adapter" do |adapter|
       end
     end
 
-    it "should return create_view in dump stream" do 
+    it "should return create_view in dump stream" do
       stream = StringIO.new
       ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
       _(stream.string).must_match(/create_view/)
     end
 
     if ActiveRecord::Base.connection.supports_materialized_views?
-      it "should return create_materialized_view in dump stream" do 
+      it "should return create_materialized_view in dump stream" do
         stream = StringIO.new
         ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
         _(stream.string).must_match(/create_materialized_view/)
       end
 
-      it 'should return add_index in dump stream' do
+      it "should return add_index in dump stream" do
         stream = StringIO.new
         ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
         _(stream.string).must_match(/add_index/)
@@ -62,7 +62,7 @@ shared_examples_for "an adapter" do |adapter|
       end
     end
 
-    it "should rebuild views in dump stream" do 
+    it "should rebuild views in dump stream" do
       stream = StringIO.new
       ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, stream)
 
@@ -93,39 +93,39 @@ shared_examples_for "an adapter" do |adapter|
   describe "#create_view" do
     let(:view_name) { :view_name }
 
-    it "throws error when block not given and no build_query" do 
+    it "throws error when block not given and no build_query" do
       _(lambda { shared_base.create_view(view_name) }).must_raise(RuntimeError)
     end
 
     describe "view_name" do
-      it "takes a symbol as the view_name" do 
+      it "takes a symbol as the view_name" do
         _(shared_base.create_view(view_name.to_sym, Product.all)).must_match(/#{view_name}/)
       end
 
-      it "takes a string as the view_name" do 
+      it "takes a string as the view_name" do
         _(shared_base.create_view(view_name.to_s, Product.all)).must_match(/#{view_name}/)
       end
     end
 
-    describe "build_query" do 
-      it "uses a string if passed" do 
+    describe "build_query" do
+      it "uses a string if passed" do
         select_statement = "SELECT * FROM products"
         _(shared_base.create_view(view_name, select_statement)).must_match(/#{Regexp.escape(select_statement)}/)
       end
 
-      it "uses an Arel::Relation if passed" do 
+      it "uses an Arel::Relation if passed" do
         select_statement = Product.all.to_sql
         _(shared_base.create_view(view_name, Product.all)).must_match(/#{Regexp.escape(select_statement)}/)
       end
     end
 
-    describe "block" do 
-      it "can use an Arel::Relation from the yield" do 
+    describe "block" do
+      it "can use an Arel::Relation from the yield" do
         select_statement = Product.all.to_sql
         _(shared_base.create_view(view_name) { Product.all }).must_match(/#{Regexp.escape(select_statement)}/)
       end
 
-      it "can use a String from the yield" do 
+      it "can use a String from the yield" do
         select_statement = "SELECT * FROM products"
         _(shared_base.create_view(view_name) { "SELECT * FROM products" }).must_match(/#{Regexp.escape(select_statement)}/)
       end
@@ -136,11 +136,11 @@ shared_examples_for "an adapter" do |adapter|
     let(:view_name) { :view_name }
 
     describe "view_name" do
-      it "takes a symbol as the view_name" do 
+      it "takes a symbol as the view_name" do
         _(shared_base.drop_view(view_name.to_sym)).must_match(/#{view_name}/)
       end
 
-      it "takes a string as the view_name" do 
+      it "takes a string as the view_name" do
         _(shared_base.drop_view(view_name.to_s)).must_match(/#{view_name}/)
       end
     end
