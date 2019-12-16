5544c556d21e21abcf989ad904da8dcda4a3d056
Mark Oveson <mark.oveson@mx.com>
2019-12-16 10:59:55 -0700

    Update mysql2 adapter to handle results returned by jdbc

--- lib/spectacles/schema_statements/mysql2_adapter.rb
+++ lib/spectacles/schema_statements/mysql2_adapter.rb
@@ -18,17 +18,21 @@ module Spectacles
         SQL
 
         execute_and_free(sql, 'SCHEMA') do |result|
-          result.collect(&:first)
+          result.respond_to?(:rows) ? result.rows.map(&:first) : result.map(&:first)
         end
       end
 
       def views(name = nil) #:nodoc:
-        execute("SHOW FULL TABLES WHERE TABLE_TYPE='VIEW'").map { |row| row[0] }
+        result = execute("SHOW FULL TABLES WHERE TABLE_TYPE='VIEW'")
+
+        result.respond_to?(:rows) ? result.rows.map(&:first) : result.map(&:first)
       end
 
       def view_build_query(view, name = nil)
-        row = execute("SHOW CREATE VIEW #{view}", name).first
-        return row[1].gsub(/CREATE .*? (AS)+/i, "")
+        result = execute("SHOW CREATE VIEW #{view}", name)
+        algorithm_string = result.respond_to?(:rows) ? result.rows.first[1] : result.first[1]
+
+        algorithm_string.gsub(/CREATE .*? (AS)+/i, "")
       rescue ActiveRecord::StatementInvalid => e
         raise "No view called #{view} found, #{e}"
       end
