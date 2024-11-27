a804e78c736dc0484425b3561d82ba25cf2bfcfe
Adam Hutchison <adam.hutchison@hey.com>
2024-11-27 11:52:16 -0700

    Remove #tables overrides
    Rails now handles filtering views from the tables query, so these
    overrides are no longer needed.

--- lib/spectacles/schema_statements/mysql2_adapter.rb
+++ lib/spectacles/schema_statements/mysql2_adapter.rb
@@ -3,25 +3,6 @@ module Spectacles
     module Mysql2Adapter
       include Spectacles::SchemaStatements::AbstractAdapter
 
-      # overrides the #tables method from ActiveRecord's MysqlAdapter
-      # to return only tables, and not views.
-      def tables(name = nil, database = nil, like = nil)
-        database = database ? quote_table_name(database) : "DATABASE()"
-        by_name = like ? "AND table_name LIKE #{quote(like)}" : ""
-
-        sql = <<-SQL.squish
-          SELECT table_name, table_type
-            FROM information_schema.tables
-           WHERE table_schema = #{database}
-             AND table_type = 'BASE TABLE'
-             #{by_name}
-        SQL
-
-        execute_and_free(sql, "SCHEMA") do |result|
-          rows_from(result).map(&:first)
-        end
-      end
-
       def views(name = nil) # :nodoc:
         result = execute("SHOW FULL TABLES WHERE TABLE_TYPE='VIEW'")
 
